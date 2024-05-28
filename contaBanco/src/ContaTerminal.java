534ac7053d4b94a877914ccbf7ea0c548e6d3442
fernandopix <fernando_pix@hotmail.com>
2024-05-28 17:43:06 -0300

    exercico simulador de conta bancária

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+    
+          
+    Scanner scanner = new Scanner(System.in);
+
+        // Solicitar e ler o número da conta
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        // Solicitar e ler a agência
+        System.out.print("Por favor, digite o número da Agência: ");
+        scanner.nextLine(); // Consumir a nova linha deixada pelo nextInt()
+        String agencia = scanner.nextLine();
+
+        // Solicitar e ler o nome do cliente
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        // Solicitar e ler o saldo
+        System.out.print("Por favor, digite o saldo da Conta: ");
+        double saldo = scanner.nextDouble();
+
+        // Exibir a mensagem final com as informações inseridas pelo usuário
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        // Fechar o scanner
+        scanner.close();
     }
 }
